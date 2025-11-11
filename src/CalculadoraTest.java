0df42ca1df350684449db263b3d1e4a91fd98595
unknown <eruiz@iespabloserrano.com>
2025-11-11 23:34:56 +0100

    aaaa

--- src/CalculadoraTest.java
+++ src/CalculadoraTest.java
@@ -2,7 +2,7 @@ public class CalculadoraTest {
     public static void main(String[] args) {
         Calculadora calc = new Calculadora();
 
-        #si no pasa los test, genera un AssertionError y no muestra el sout
+        //si no pasa los test, genera un AssertionError y no muestra el sout
 		assert calc.sumar(2, 3) == 5 : "Error en sumar()";
         assert calc.restar(5, 2) == 3 : "Error en restar()";
 
